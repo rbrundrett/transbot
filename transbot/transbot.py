dec12d0c62aeab4bb00713cbf6eff54d05ad9e16
rbrundrett <bob@bobbrundrett.com>
2017-07-05 15:17:37 -0400

    split "alify" function to a new module

--- transbot/transbot.py
+++ transbot/transbot.py
@@ -5,6 +5,7 @@ import random
 from slackclient import SlackClient
 from mstranslator import Translator
 import config
+import alify
 
 #initialize translator API
 translator = Translator(config.MSTRANS_ID)
@@ -27,63 +28,7 @@ EXAMPLE_COMMAND = "do"
 TRANS_COMMAND = "nazify"
 
 
-def alify(command, channel):
-    """
-        Receives commands directed at the bot and determines if they
-        are valid commands. If so, then acts on the commands. If not,
-        returns back what it needs for clarification.
-    """
-    ' '.join(i.capitalize() for i in command.split(' '))
-    xs = command.split(" ")
-    for x in xs:
-        if  x.lower()=="laptop":
-            new_word=random.sample(["LAB-top","Lab=Top","lab Top","LAB_TOP"],1)[0]
-            xs[xs.index(x)]=new_word
-            x=new_word
-        if "mind" in x.lower():
-            new_word=x.lower()
-            new_word=new_word.replace("mind","mine")
-            xs[xs.index(x)]=new_word
-            x=new_word
-        #add random hyphen
-        if random.randint(1, 10)<2:
-            i=random.randint(1,len(x))
-            new_word=x[:i] + "-" + x[i:]
-            xs[xs.index(x)]=new_word
-            x=new_word
-        #add random apostrophe
-        if random.randint(1, 100)<6:
-            i=random.randint(1,len(x))
-            new_word=x[:i] + "'" + x[i:]
-            xs[xs.index(x)]=new_word
-            x=new_word
-        #add random quotes
-        if random.randint(1, 10)<2:
-            new_word='"' + x + '"'
-            xs[xs.index(x)]=new_word
-            x=new_word
-        #add random casing
-        if random.randint(1, 10)<6:
-            new_word=x.title()
-            xs[xs.index(x)]=new_word
-            x=new_word
-        #add random upper case
-        if random.randint(1, 100)<6:
-            new_word=x.upper()
-            xs[xs.index(x)]=new_word
-            x=new_word
-        #add random commas
-        if random.randint(1, 100)<5 or x[-1:]==",":
-            new_word=x + "," * random.randint(1,10)
-            xs[xs.index(x)]=new_word
-            x=new_word
-    response = " ".join(xs)
-    if command == "version":
-        response = "transbot version 1.3"
 
-    #slack_client.api_call("chat.postMessage", channel=channel,
-    #                      text=response, as_user=True)
-    return response
 
 def handle_trans_cmd(command, channel):
     """
